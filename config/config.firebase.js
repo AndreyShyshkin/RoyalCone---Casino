563c46a45502ac9d84942ad556e1f0043bd00a0f
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2024-07-08 14:20:34 +0300

    env set

--- config/config.firebase.js
+++ config/config.firebase.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import dotenv from 'dotenv'
 import { getAnalytics } from 'firebase/analytics'
+import { initializeApp } from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+// Load environment variables
+dotenv.config()
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'QWERTY1234567890',
-	authDomain: 'name.firebaseapp.com',
-	projectId: 'name',
-	storageBucket: 'name.appspot.com',
-	messagingSenderId: '1234567890',
-	appId: '1:1234567890:web:qwerty1234567890',
-	measurementId: 'G-QWER1234',
+	apiKey: process.env.FIREBASE_apiKey,
+	authDomain: process.env.FIREBASE_authDomain,
+	projectId: process.env.FIREBASE_projectId,
+	storageBucket: process.env.FIREBASE_storageBucket,
+	messagingSenderId: process.env.FIREBASE_messagingSenderId,
+	appId: process.env.FIREBASE_appId,
+	measurementId: process.env.FIREBASE_measurementId,
 }
 
 // Initialize Firebase
--- config/config.telegram.js
+++ config/config.telegram.js
@@ -1,13 +1,18 @@
+import dotenv from 'dotenv'
 import TelegramBot from 'node-telegram-bot-api'
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN'
+// Load environment variables
+dotenv.config()
 
+// Use process.env to access environment variables
+const token = process.env.TELEGRAM_token
+const webAppUrl = 'http://192.168.1.135:5173/'
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true })
 
 // Listen for any kind of message. There are different kinds of
 // messages.
+
 bot.on('message', msg => {
 	const chatId = msg.chat.id
 
